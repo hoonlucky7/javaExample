14148547b64842155521fee91d7c1bb728aa409b
hoonlucky7 <hoon.lucky7@gmail.com>
2019-09-20 21:10:42 +0900

    OperatorEx1
    Signed-off-by: hoonlucky7 <hoon.lucky7@gmail.com>

---
+++ OperatorEx1.java
@@ -0,0 +1,10 @@
+class OperatorEx1 {
+    public static void main(String args[]) {
+        int i = 5;
+        i++;
+        System.out.println(i);
+        int j = 5;
+        j = j + 1;
+        System.out.println(j);
+    }
+}
